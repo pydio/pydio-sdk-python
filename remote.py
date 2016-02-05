5e1140ae5c27e1be84168982ac174dd95c334bf7
cdujeu <charles@ajaxplorer.info>
2016-02-05 11:46:08 +0100

    Fix sharing

--- remote.py
+++ remote.py
@@ -1040,7 +1040,6 @@ class PydioSdk():
     def share(self, ws_label, ws_description, password, expiration, downloads, can_read, can_download, paths,
                     link_handler, can_write):
         data = dict()
-        #data["get_action"] = "share"
         data["sub_action"] = "create_minisite"
         data["guest_user_pass"] = password
         data["create_guest_user"] = "true"
@@ -1049,15 +1048,14 @@ class PydioSdk():
         data["downloadlimit"] = downloads
         data["repo_description"] = ws_description
         data["repo_label"] = ws_label
-        #data["nodes[]"] = paths
         data["custom_handle"] = link_handler
-        #data["file"] = paths
-        #data["dir"] = os.path.dirname(paths)
 
         if can_download == "true":
             data["simple_right_download"] = "on"
         if can_read == "true":
             data["simple_right_read"] = "on"
+        else:
+            data["minisite_layout"] = "ajxp_unique_dl"
         if can_write == "true":
             data["simple_right_write"] = "on"
 
