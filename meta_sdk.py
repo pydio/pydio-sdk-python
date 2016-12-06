f94876dd86fbfe82fa7fc2294c1c7cbf2e454cb6
cdujeu <charles@ajaxplorer.info>
2016-12-06 11:52:31 +0100

    new sdk for meta related calls

---
+++ meta_sdk.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2016 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+#  This file is part of Pydio.
+#
+#  Pydio is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pydio is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  The latest code can be found at <http://pyd.io/>.
+#
+
+from remote import PydioSdk
+
+
+class MetaSdk(PydioSdk):
+
+    def edit_user_meta(self, file_path, meta_data_dict):
+        return self.perform_request(self.url+'/edit_user_meta'+file_path, 'post', meta_data_dict)
